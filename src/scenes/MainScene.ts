15e304786d4fb2348b9918debb9ce22ea12add3b
DeltaMokey <engincanozkan@outlook.com>
2024-02-13 14:32:49 +0300

    Code review

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,56 +1,35 @@
 import Phaser from 'phaser';
 
+const GROUND_KEY = 'ground';
+const DUDE_KEY = 'dude';
+
 export default class MainScene extends Phaser.Scene {
 
-    private platforms!: Phaser.Physics.Arcade.StaticGroup;
-    private player: Phaser.Physics.Arcade.Sprite;
+    private platforms: Phaser.Physics.Arcade.StaticGroup;
+    private player?: Phaser.Physics.Arcade.Sprite;
     private cursor?: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super('MainScene');
+
+        this.player = undefined;
+        this.cursor = undefined;
     }
 
     preload() {
         this.load.image('bomb', 'assets/bomb.png');
-        this.load.image('ground', 'assets/platform.png');
+        this.load.image(GROUND_KEY, 'assets/platform.png');
         this.load.image('sky', 'assets/sky.png');
         this.load.image('star', 'assets/star.png');
-        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet(DUDE_KEY, 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
     create() {
         this.add.image(400, 300, 'sky');
-        this.platforms = this.physics.add.staticGroup();
-        const ground1: Phaser.Types.Physics.Arcade.SpriteWithStaticBody = this.physics.add.staticSprite(400, 568, 'ground').setScale(2).refreshBody();
-        this.platforms.add(ground1);
-
-        this.platforms.create(600, 400, 'ground');
-        this.platforms.create(50, 250, 'ground');
-        this.platforms.create(750, 220, 'ground');
-
-        this.player = this.physics.add.sprite(100, 450, 'dude');
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3} ),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [ {key: 'dude', frame: 4} ],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 } ),
-            frameRate: 10,
-            repeat: -1
-        });
+        // this.add.image(400, 300, 'star')
+        
+        this.platforms = this.createPlatforms();
+        this.player = this.createPlayer();
 
         this.physics.add.collider(this.player, this.platforms);
 
@@ -78,5 +57,46 @@ export default class MainScene extends Phaser.Scene {
         {
             this.player?.setVelocityY(-330);
         }
+    }
+
+    private createPlatforms(): Phaser.Physics.Arcade.StaticGroup {
+        const platforms = this.physics.add.staticGroup();
+        const ground1: Phaser.Types.Physics.Arcade.SpriteWithStaticBody = 
+            this.physics.add.staticSprite(400, 568, GROUND_KEY).setScale(2).refreshBody();
+        platforms.add(ground1);
+
+        platforms.create(600, 400, GROUND_KEY);
+        platforms.create(50, 250, GROUND_KEY);
+        platforms.create(750, 220, GROUND_KEY);
+
+        return platforms;
+    }
+
+    private createPlayer(): Phaser.Types.Physics.Arcade.SpriteWithDynamicBody {
+        const player = this.physics.add.sprite(100, 450, DUDE_KEY);
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 0, end: 3} ),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ {key: DUDE_KEY, frame: 4} ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 5, end: 8 } ),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        return player;
     }
 }
